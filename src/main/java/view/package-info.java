7b20f98e5ec03e587ad2b0155c62a45129113bf4
Jakub Zaremba <jzaremba@MAgiC.local>
2020-12-12 13:36:24 +0100

    GUI done in swing

--- src/main/java/view/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * View package - responsible for displaying information / data
- */
-
-package view;
---
+++ src/main/java/zOLDview/View.java
@@ -0,0 +1,40 @@
+package zOLDview;
+import java.util.List;
+/**
+* Class responsible for displaying data on the screen 
+*
+* @author Jakub Zaremba
+* @version 2.0
+* @since   2020-11-14
+*/
+public class View {
+    /**
+     * @deprecated
+     * Simple method that display give message, replaced with ConsoleMessage which can take more arguments
+     * @param message information that you want to display on the screen
+     */
+    @Deprecated
+    public void displayMessage(String message){
+        System.out.println(message);
+    }
+    
+    /**
+     * Method allows you to display message in console, and since it has changeable numbers of parameters you can display more than one message. 
+     * @param message String array - contains messages you want to display.
+     */
+    public void ConsoleMessage(String... message){
+        for(int i=0;i<message.length;i++)
+            System.out.println("Game console message - "+message[i]);
+    }
+    /**
+     * Method that display dice polls 
+     * @param dices integer array with dice polls
+     */
+    public void showDicesPolls(List<Integer> dices){
+        System.out.print("Game console message - ");
+        for (Integer dice : dices) {
+            System.out.print(dice+ ", ");
+        }
+        System.out.println("");
+    }
+}
